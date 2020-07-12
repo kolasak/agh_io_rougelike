0db15e73d91f18a94eac9ca4b5df24b941cfebdc
Tomasz Skrzek <tomasz.skrzek.98@gmail.com>
2020-07-12 16:17:14 +0200

    IG-5 refactor + interacting with field on "r"

--- character/CharacterControl.py
+++ character/CharacterControl.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import KEYDOWN, K_w, K_a, K_s, K_d, K_e
+from pygame.locals import KEYDOWN, K_w, K_a, K_s, K_d, K_e, K_r
 
 from enums.Direction import Direction
 from graphics import Screen
@@ -11,47 +11,34 @@ class CharacterControl:
 
     @staticmethod
     def execute_character_movement(event_key, character_info_view, fields):
+        old_x = character_info_view.character_info.x
+        old_y = character_info_view.character_info.y
+
         if event_key == K_w:
             if CharacterControl.check_if_passable(fields[character_info_view.character_info.x][character_info_view.character_info.y-1]):
-                old_x = character_info_view.character_info.x
-                old_y = character_info_view.character_info.y
                 character_info_view.character_info.y = character_info_view.character_info.y-1
                 Screen.Screen.render_character(old_x, old_y, character_info_view.character_info.x, character_info_view.character_info.y, character_info_view.character_img, Direction.NORTH)
-            else:
-                print("not passable")
         if event_key == K_a:
             if CharacterControl.check_if_passable(fields[character_info_view.character_info.x-1][character_info_view.character_info.y]):
-                old_x = character_info_view.character_info.x
-                old_y = character_info_view.character_info.y
                 character_info_view.character_info.x = character_info_view.character_info.x-1
                 Screen.Screen.render_character(old_x, old_y, character_info_view.character_info.x, character_info_view.character_info.y, character_info_view.character_img, Direction.WEST)
-            else:
-                print("not passable")
         if event_key == K_d:
             if CharacterControl.check_if_passable(fields[character_info_view.character_info.x+1][character_info_view.character_info.y]):
-                old_x = character_info_view.character_info.x
-                old_y = character_info_view.character_info.y
                 character_info_view.character_info.x = character_info_view.character_info.x+1
                 Screen.Screen.render_character(old_x, old_y, character_info_view.character_info.x, character_info_view.character_info.y, character_info_view.character_img, Direction.EAST)
-            else:
-                print("not passable")
         if event_key == K_s:
             if CharacterControl.check_if_passable(fields[character_info_view.character_info.x][character_info_view.character_info.y+1]):
-                old_x = character_info_view.character_info.x
-                old_y = character_info_view.character_info.y
                 character_info_view.character_info.y = character_info_view.character_info.y+1
                 Screen.Screen.render_character(old_x, old_y, character_info_view.character_info.x, character_info_view.character_info.y, character_info_view.character_img, Direction.SOUTH)
-            else:
-                print("not passable")
         if event_key == K_e:
             field = fields[character_info_view.character_info.x][character_info_view.character_info.y]
             if field.item is not None:
                 character_info_view.character_info.add_item(field.get_item())
                 Screen.Screen.render_character(character_info_view.character_info.x, character_info_view.character_info.y, character_info_view.character_info.x, character_info_view.character_info.y, character_info_view.character_img, Direction.SOUTH)
                 Screen.Screen.display_character_info(character_info_view)
-
+        if event_key == K_r:
+            fields[character_info_view.character_info.x][character_info_view.character_info.y].interact()
 
     @staticmethod
     def check_if_passable(field):
-        print(field.colour)
         return field.passable
