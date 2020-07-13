51edf2f2298145ec1424882ba344d324be0bab4b
Krzysztof Kolasa <kyskolasa2@gmail.com>
2020-07-13 18:16:45 +0200

    [IG-14] Add parsing of npc configuration

--- tokens/NpcToken.py
+++ tokens/NpcToken.py
@@ -1,7 +1,27 @@
 from tokens.Token import Token
-
+from controllers.DialogController import DialogController
 
 class NpcToken(Token):
+    def __init__(self, name, image, attributes, dialog):
+        self._name = name
+        self._image = image
+        self._attributes = attributes
+        self._dialog = dialog
+
+    @property
+    def image(self):
+        return self._image
+
     def interact(self, character_info=None):
-        # talk
-        pass
+        controller = DialogController(character_info, self)
+        return controller.start_dialog()
+
+    def name(self):
+        return self._name
+
+    def attributes(self):
+        return self._attributes
+    
+    def dialog(self):
+        return self._dialog
+
