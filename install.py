da6f2567023a8b465c1c6420066343c0e99b29f1
Mikołaj Tomalik <tomalikem@op.pl>
2020-07-17 10:46:59 +0200

    instalation destination name changed

--- install.py
+++ install.py
@@ -4,13 +4,13 @@ from shutil import copyfile, rmtree
 
 if __name__ == "__main__":
     os.system('cmd /c "pyinstaller main.py --onefile"')
-    if not os.path.exists("app"):
-        os.mkdir("app")
-    copyfile("dist/main.exe", "app/jakir.exe")
-    copy_tree("images", "app/images")
-    copyfile("config.json", "app/config.json")
-    copyfile("riddles.json", "app/riddles.json")
-    copyfile("freesansbold.ttf", "app/freesansbold.ttf")
+    if not os.path.exists("jakir"):
+        os.mkdir("jakir")
+    copyfile("dist/main.exe", "jakir/jakir.exe")
+    copy_tree("images", "jakir/images")
+    copyfile("config.json", "jakir/config.json")
+    copyfile("riddles.json", "jakir/riddles.json")
+    copyfile("freesansbold.ttf", "jakir/freesansbold.ttf")
 
     rmtree("build")
     rmtree("dist")
